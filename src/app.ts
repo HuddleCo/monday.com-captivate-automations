d289f5a5761e4907725b34cc623f797c4c4a6966
David Milanese <milanese.david@gmail.com>
2021-06-10 19:02:38 +0930

    Display the version when starting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+const { version } = require('../package.json');
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const port = process.env.PORT || 80;
 
 app.use(routes);
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Running version: ${version}`);
+  console.log(`App listening at http://localhost:${port}`);
+});
 
 export default app;

38682a85b485f278464ef4fe9b3188f4e92b83d2
David Milanese <milanese.david@gmail.com>
2021-06-10 17:05:16 +0930

    remove depricated code

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import routes from './routes';
 
 dotenv.config();
@@ -8,7 +7,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 80;
 
-app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(port, () => console.log(`Quickstart app listening at http://localhost:${port}`));

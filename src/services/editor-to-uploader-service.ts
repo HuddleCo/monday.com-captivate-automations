fc071a69d03dc7bdb11b29874258c48b7300b6f3
David Milanese <milanese.david@gmail.com>
2021-06-15 12:35:29 +0930

    update tuesday service

--- src/services/editor-to-uploader-service.ts
+++ src/services/editor-to-uploader-service.ts
@@ -1,27 +1,13 @@
-import initMondayClient from "monday-sdk-js";
-import { OptionsType, Response } from "../types";
+import { performQuery } from "./queryCounter";
 
-class EditorToUploaderService {
-  private mondayClient;
-
-  private queryCounter: number;
-
-  constructor(apiToken: string) {
-    this.mondayClient = initMondayClient();
-    this.mondayClient.setToken(apiToken);
-
-    this.queryCounter = 0;
-  }
-
-  async process(
-    boardId: number,
-    status: string,
-    itemId: number
-  ): Promise<string> {
-    return new Promise((resolve) =>
-      resolve(`${this.mondayClient} - ${boardId} - ${status} - ${itemId}`)
-    );
-  }
-}
-
-export default EditorToUploaderService;
+export default async (
+  token: string,
+  boardId: number,
+  status: string,
+  itemId: number
+): Promise<string> => {
+  await performQuery(token, "", {});
+  return new Promise((resolve) =>
+    resolve(`${token} - ${boardId} - ${status} - ${itemId}`)
+  );
+};

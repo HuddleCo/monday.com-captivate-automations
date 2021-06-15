70cb8562ec3e2aeee9590a667bc18d6ac89f4ca2
David Milanese <milanese.david@gmail.com>
2021-06-15 12:49:57 +0930

    refactor monday service to 1 connection

--- src/controllers/monday-controller.ts
+++ src/controllers/monday-controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, RequestHandler } from "express";
 
-import MondayService from "../services/monday-service";
+import connection from "../services/monday-service";
 import { unmarshal } from "../middlewares/authentication";
 
 export const executeAction: RequestHandler = (req: Request, res: Response) => {
@@ -9,13 +9,11 @@ export const executeAction: RequestHandler = (req: Request, res: Response) => {
 
   return !shortLivedToken
     ? res.status(500).send({ message: "shortLivedToken is not provided" })
-    : new MondayService(shortLivedToken)
-        .createContentFromEpisodeToBoard(shortLivedToken, itemId, targetBoardId)
-        .then(
-          (message) => res.status(200).send({ message }),
-          (err) => {
-            console.error(err);
-            return res.status(500).send({ message: "internal server error" });
-          }
-        );
+    : connection(shortLivedToken, itemId, targetBoardId).then(
+        (message) => res.status(200).send({ message }),
+        (err) => {
+          console.error(err);
+          return res.status(500).send({ message: "internal server error" });
+        }
+      );
 };
--- src/services/monday-service.ts
+++ src/services/monday-service.ts
@@ -3,28 +3,18 @@ import { getContentFor } from "./getContentFor";
 import { createGroup } from "./createGroup";
 import { createItemFromItem } from "./createItemFromItem";
 
-class MondayService {
-  private token: string;
+export default async (
+  token: string,
+  episodeId: number,
+  targetBoardId: number
+): Promise<string> => {
+  const episode = await getItem(token, episodeId);
+  const group = await createGroup(token, targetBoardId, episode);
+  await Promise.all(
+    getContentFor(episode).map((content) =>
+      createItemFromItem(token, targetBoardId, group, content, episode)
+    )
+  );
 
-  constructor(token: string) {
-    this.token = token;
-  }
-
-  async createContentFromEpisodeToBoard(
-    token: string,
-    episodeId: number,
-    targetBoardId: number
-  ): Promise<string> {
-    const episode = await getItem(token, episodeId);
-    const group = await createGroup(token, targetBoardId, episode);
-    await Promise.all(
-      getContentFor(episode).map((content) =>
-        createItemFromItem(token, targetBoardId, group, content, episode)
-      )
-    );
-
-    return `Created content for ${episode.name}`;
-  }
-}
-
-export default MondayService;
+  return `Created content for ${episode.name}`;
+};

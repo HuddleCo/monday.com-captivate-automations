ed302310108dc5b25edb055af293b38f5078e7cb
David Milanese <milanese.david@gmail.com>
2021-06-17 02:31:22 +0930

    remove unused files

--- src/services/json-column-values.ts
+++
@@ -1,5 +0,0 @@
-import { ItemType } from "../types";
-import { columnValuesForCreatingEpsiode } from "./column-values-for-creating-epsiode";
-
-export const jsonColumnValues = (item: ItemType): string =>
-  JSON.stringify(columnValuesForCreatingEpsiode(item));

33a1ab8429f632a6563c28c160a323973a3127d2
David Milanese <milanese.david@gmail.com>
2021-06-16 18:05:20 +0930

    shortLivedToken can not be blank

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -9,12 +9,12 @@ type OpenId = {
   // accountId: string /* Unused property */;
   // userId: string /* Unused property */;
   // backToUrl: string | undefined; /* Unused property */
-  shortLivedToken: string | undefined;
+  shortLivedToken: string;
 };
 
-const isOpenId = (maybeSession: Partial<OpenId>): maybeSession is OpenId => {
-  const session = maybeSession as OpenId;
-  return typeof session.shortLivedToken === "string";
+const isOpenId = (maybeOpenId: Partial<OpenId>): maybeOpenId is OpenId => {
+  const openId = maybeOpenId as OpenId;
+  return typeof openId.shortLivedToken === "string";
 };
 
 export const unmarshal = (request: Request): OpenId => {
@@ -32,7 +32,7 @@ export const unmarshal = (request: Request): OpenId => {
     process.env.MONDAY_SIGNING_SECRET
   ) as Partial<OpenId>;
 
-  if (!isOpenId(payload)) throw new Error("Payload is not of type OpenId");
+  if (!isOpenId(payload)) throw new MissingShortLivedTokenError();
 
   return payload;
 };
@@ -49,15 +49,15 @@ export const authenticationMiddleware = (
       if (err instanceof NoCredentialsError) {
         res
           .status(401)
-          .json({ error: "not authenticated, no credentials in request" });
+          .send({ error: "not authenticated, no credentials in request" });
       } else if (err instanceof MissingMondaySigningSecretError) {
-        res.status(500).json({
+        res.status(500).send({
           error: "Missing MONDAY_SIGNING_SECRET (should be in .env file)",
         });
       } else if (err instanceof MissingShortLivedTokenError) {
-        res.status(500).json({ message: "shortLivedToken is not provided" });
+        res.status(500).send({ message: "shortLivedToken is not provided" });
       } else {
-        res.status(401).json({
+        res.status(401).send({
           error: "authentication error, could not verify credentials",
         });
       }

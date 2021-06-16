3fe8cd65f61f5868d8508b0d6dff124e69c48eac
David Milanese <milanese.david@gmail.com>
2021-06-16 15:18:26 +0930

    refactor authentication with appropriate typechecking

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -3,50 +3,62 @@ import { NextFunction, Request, Response } from "express";
 
 import NoCredentialsError from "../errors/no-credentials-error";
 import MissingMondaySigningSecretError from "../errors/missing-monday-signing-secret-error";
+import MissingShortLivedTokenError from "../errors/missing-short-lived-token-error";
 
-type Session = {
-  accountId: string;
-  userId: string;
-  backToUrl: string | undefined;
+type OpenId = {
+  // accountId: string; /* Unused property */
+  // userId: string; /* Unused property */
+  // backToUrl: string | undefined; /* Unused property */
   shortLivedToken: string | undefined;
 };
 
-export const unmarshal = (request: Request): Session => {
+export const unmarshal = (request: Request): OpenId => {
   const { authorization } = request.headers;
-  if (typeof authorization !== "string") throw new NoCredentialsError();
+  if (typeof authorization !== "string") {
+    throw new NoCredentialsError();
+  }
 
   if (typeof process.env.MONDAY_SIGNING_SECRET !== "string") {
     throw new MissingMondaySigningSecretError();
   }
 
-  return jwt.verify(
+  const maybeSession = jwt.verify(
     authorization,
     process.env.MONDAY_SIGNING_SECRET
-  ) as Session;
+  );
+
+  if (typeof maybeSession === "string")
+    throw new Error("session can not be a string");
+
+  const session = maybeSession as Record<string, unknown>;
+
+  if (!session.shortLivedToken) throw new MissingShortLivedTokenError();
+
+  return session as OpenId;
 };
 
 export const authenticationMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
-): void => {
-  try {
-    unmarshal(req);
-
-    next();
-  } catch (err) {
-    if (err instanceof NoCredentialsError) {
-      res
-        .status(401)
-        .json({ error: "not authenticated, no credentials in request" });
-    } else if (err instanceof MissingMondaySigningSecretError) {
-      res.status(500).json({
-        error: "Missing MONDAY_SIGNING_SECRET (should be in .env file)",
-      });
-    } else {
-      res
-        .status(401)
-        .json({ error: "authentication error, could not verify credentials" });
-    }
-  }
-};
+): Promise<void> =>
+  Promise.resolve(req)
+    .then(unmarshal)
+    .then(next)
+    .catch((err) => {
+      if (err instanceof NoCredentialsError) {
+        res
+          .status(401)
+          .json({ error: "not authenticated, no credentials in request" });
+      } else if (err instanceof MissingMondaySigningSecretError) {
+        res.status(500).json({
+          error: "Missing MONDAY_SIGNING_SECRET (should be in .env file)",
+        });
+      } else if (err instanceof MissingShortLivedTokenError) {
+        res.status(500).json({ message: "shortLivedToken is not provided" });
+      } else {
+        res.status(401).json({
+          error: "authentication error, could not verify credentials",
+        });
+      }
+    });

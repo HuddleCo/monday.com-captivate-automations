37403b40d7aafad0baa34687bd63a0dfead3ac3d
David Milanese <milanese.david@gmail.com>
2021-06-17 02:31:16 +0930

    refactor for code reuse

--- src/connectors/create-content-for-episode-connector.ts
+++ src/connectors/create-content-for-episode-connector.ts
@@ -3,8 +3,17 @@ import { getItem } from "../monday-api/queries/get-item";
 import { episodeContents } from "../services/episode-contents";
 import { createGroup } from "../monday-api/queries/create-group";
 import { clientNameForEpisode } from "../services/client-name-for-episode";
-import { columnValuesForCreatingEpsiode } from "../services/column-values-for-creating-epsiode";
 import { createItem } from "../monday-api/queries/create-item";
+import { getBoard } from "../monday-api/queries/get-board";
+import { cloneItemColumnsForBoard } from "../services/clone-item-columns-for-board";
+import {
+  ASSET_TYPE_COLUMN_TITLE,
+  EPISODE_NAME_COLUMN_TITLE,
+} from "../constants";
+import type { BoardType } from "../types";
+
+const columnId = (board: BoardType, columnTitle: string): string =>
+  board.columns.find(({ title }) => title === columnTitle)?.id || "Title";
 
 export default async (
   client: MondayClient,
@@ -13,6 +22,7 @@ export default async (
 ): Promise<string> => {
   const episode = await getItem(client, episodeId);
   const group = await createGroup(client, targetBoardId, episode);
+  const board = await getBoard(client, targetBoardId);
 
   await Promise.all(
     episodeContents(episode).map((content) =>
@@ -22,9 +32,11 @@ export default async (
         group.id,
         `${content} - ${clientNameForEpisode(episode)}`,
         JSON.stringify({
-          ...columnValuesForCreatingEpsiode(episode),
-          status_1: { label: content },
-          status_17: { label: content },
+          ...cloneItemColumnsForBoard(episode, board),
+          [columnId(board, EPISODE_NAME_COLUMN_TITLE)]: {
+            item_ids: [episode.id],
+          },
+          [columnId(board, ASSET_TYPE_COLUMN_TITLE)]: { label: content },
         })
       )
     )

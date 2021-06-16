b2bae936f452493f4b144118b2700346f321b2f1
David Milanese <milanese.david@gmail.com>
2021-06-16 12:30:00 +0930

    Create a sample item when all content are complete

--- src/services/editor-to-uploader-service.ts
+++ src/services/editor-to-uploader-service.ts
@@ -1,4 +1,5 @@
 import { allItemsComplete as allItemsMatch } from "./allItemsComplete";
+import { createItem } from "./createItem";
 import { getItem } from "./getItem";
 import { getItemsInGroupContainingItem } from "./getItemsInGroupContainingItem";
 
@@ -14,6 +15,8 @@ export default async (
 
   if (!allItemsMatch(items, statusColumnId, status))
     return `Some episodes are not ${status}. No go buddy`;
+
+  await createItem(token, boardId, "", "Sample", item);
 
   return `All episodes are ${status}. Autobots roll-out!`;
 };

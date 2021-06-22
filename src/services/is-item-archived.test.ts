fae3b45b3ef469d4134ff90fe580b7e4b310099c
David Milanese <milanese.david@gmail.com>
2021-06-23 01:23:40 +0930

    test: update test suite to use Jest

--- src/services/is-item-archived.test.ts
+++ src/services/is-item-archived.test.ts
@@ -1,26 +1,37 @@
-import { expect } from "chai";
 import { isItemArchived } from "./is-item-archived";
 
-describe("isItemArchived", () => {
-  context("when the item is archved", () => {
-    const item = {
-      id: 1,
-      state: "archived",
-      name: "Item",
-      column_values: [],
-      board: {
-        id: 2,
-        name: "Board",
-        columns: [],
-        groups: [],
-      },
-      group: {
-        id: "group_id",
-        title: "Group Title",
-        archived: false,
-      },
-    };
+const item = (state = "active") => ({
+  id: 1,
+  state,
+  name: "Item",
+  column_values: [],
+  board: {
+    id: 2,
+    name: "Board",
+    columns: [],
+    groups: [],
+  },
+  group: {
+    id: "group_id",
+    title: "Group Title",
+    archived: false,
+  },
+})
 
-    it("is true", () => expect(isItemArchived(item)).equal(true));
+describe('isItemArchived', () => {
+  describe("when items is archived", () => {
+    const archivedItem = item("archived");
+    it('when item is archived', () => {  
+      expect.hasAssertions();
+      expect(isItemArchived(archivedItem)).toBe(true)
+    });
   });
+  
+  describe("when items is active", () => {
+    const activeItem = item("activeItem");
+    it('when item is archived', () => {  
+      expect.hasAssertions();
+      expect(isItemArchived(activeItem)).toBe(false)
+    });
+  })
 });

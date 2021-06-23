55873d6c0a884c5fa9f5aed0b0e3ed4ff52254f7
David Milanese <milanese.david@gmail.com>
2021-06-23 16:10:26 +0930

    feat: add /monday/copy_podcast_item endpoint for processing episodes that require podcasts

--- src/routes/monday.ts
+++ src/routes/monday.ts
@@ -4,6 +4,7 @@ import { authenticationMiddleware } from "../middlewares/authentication";
 import * as copywriterController from "../controllers/copywriter-controller";
 import * as moveGroupController from "../controllers/move-group-controller";
 import * as moveItemController from "../controllers/move-item-controller";
+import * as podcastController from "../controllers/podcast-controller";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/monday", authenticationMiddleware);
 router.post("/monday/copywriter", copywriterController.post);
 router.post("/monday/move_group", moveGroupController.post);
 router.post("/monday/move_item", moveItemController.post);
+router.post("/monday/copy_podcast_item", podcastController.post);
 
 export default router;

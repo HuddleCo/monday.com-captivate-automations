6ceab1b1c06e0c40c7d080d41510b851995855a8
David Milanese <milanese.david@gmail.com>
2021-07-13 00:42:15 +0930

    feat: connector duplicate column automation to /monday/duplicate_column endpoint

---
+++ src/controllers/duplicate-column-controller.ts
@@ -0,0 +1,22 @@
+import * as Sentry from "@sentry/browser";
+
+import type { Request, Response, RequestHandler } from "express";
+
+import connector from "../connectors/duplicate-column-connector";
+import { unmarshal } from "../middlewares/authentication";
+import MondayApi from "../monday-api";
+
+export const post: RequestHandler = (req: Request, res: Response) =>
+  connector(
+    new MondayApi(unmarshal(req).shortLivedToken),
+    req.body.payload.inboundFieldValues.boardId,
+    req.body.payload.inboundFieldValues.itemId,
+    req.body.payload.inboundFieldValues.columnId
+  ).then(
+    (message) => res.status(200).send({ message }),
+    (err) => {
+      console.error(err);
+      Sentry.captureException(err);
+      res.status(500).send({ message: err.message });
+    }
+  );
--- src/routes/monday.ts
+++ src/routes/monday.ts
@@ -5,6 +5,7 @@ import * as copywriterController from "../controllers/copywriter-controller";
 import * as moveGroupController from "../controllers/move-group-controller";
 import * as moveItemController from "../controllers/move-item-controller";
 import * as podcastController from "../controllers/podcast-controller";
+import * as duplicateColumnController from "../controllers/duplicate-column-controller";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.post("/monday/copywriter", copywriterController.post);
 router.post("/monday/move_group", moveGroupController.post);
 router.post("/monday/move_item", moveItemController.post);
 router.post("/monday/copy_podcast_item", podcastController.post);
+router.post("/monday/duplicate_column", duplicateColumnController.post);
 
 export default router;

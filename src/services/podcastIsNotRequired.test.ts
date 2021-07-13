b6f8466d51eb385901db14012b10f5ee1a3a5d8d
David Milanese <milanese.david@gmail.com>
2021-07-13 16:45:02 +0930

    chore: prefer to use fixtures

--- src/services/podcastIsNotRequired.test.ts
+++ src/services/podcastIsNotRequired.test.ts
@@ -1,94 +1,29 @@
 import { podcastIsNotRequired } from "./podcastIsNotRequired";
 
+import { createItem } from "../connectors/createItem.test.fixtures";
+import { createColumnValues } from "../connectors/createColumnValue.test.fixtures";
+
 describe("podcastIsNotRequired", () => {
   describe("when the item does not have a Podcast column", () => {
-    const item = {
-      id: 1,
-      state: "active",
-      name: "Item",
-      column_values: [],
-      board: {
-        id: 2,
-        name: "Board",
-        columns: [],
-        groups: [],
-      },
-      group: {
-        id: "group_id",
-        title: "Group Title",
-        archived: false,
-        position: 0,
-      },
-    };
-    it("is not required for a podcast", () => {
-      expect.hasAssertions();
-      expect(podcastIsNotRequired(item)).toBe(true);
-    });
+    const item = createItem();
+
+    it("is not required for a podcast", () =>
+      expect(podcastIsNotRequired(item)).toBe(true));
   });
 
   describe("when the item does has a blank Podcast column", () => {
-    const item = {
-      id: 1,
-      state: "active",
-      name: "Item",
-      column_values: [
-        {
-          id: "2",
-          value: "{}",
-          type: "mirror",
-          text: "",
-          title: "Podcasts",
-        },
-      ],
-      board: {
-        id: 2,
-        name: "Board",
-        columns: [],
-        groups: [],
-      },
-      group: {
-        id: "group_id",
-        title: "Group Title",
-        archived: false,
-        position: 0,
-      },
-    };
-    it("is not required for a podcast", () => {
-      expect.hasAssertions();
-      expect(podcastIsNotRequired(item)).toBe(true);
-    });
+    const column = createColumnValues("", "Podcasts");
+    const item = createItem([column]);
+
+    it("is not required for a podcast", () =>
+      expect(podcastIsNotRequired(item)).toBe(true));
   });
 
   describe("when the item Podcast column has the value 1", () => {
-    const item = {
-      id: 1,
-      state: "active",
-      name: "Item",
-      column_values: [
-        {
-          id: "2",
-          value: "{}",
-          type: "mirror",
-          text: "1",
-          title: "Podcasts",
-        },
-      ],
-      board: {
-        id: 2,
-        name: "Board",
-        columns: [],
-        groups: [],
-      },
-      group: {
-        id: "group_id",
-        title: "Group Title",
-        archived: false,
-        position: 0,
-      },
-    };
-    it("is required for a podcast", () => {
-      expect.hasAssertions();
-      expect(podcastIsNotRequired(item)).toBe(false);
-    });
+    const column = createColumnValues("1", "Podcasts");
+    const item = createItem([column]);
+
+    it("is required for a podcast", () =>
+      expect(podcastIsNotRequired(item)).toBe(false));
   });
 });
